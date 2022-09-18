ba29385166928791fa1ba481ae72d8fb547d1f1f
byz18 <mackenziejaysandham@gmail.com>
2022-09-18 14:32:46 +0100

    fixfixfix

--- models/vinyl.js
+++ models/vinyl.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const path = require('path')
-const coverImageBasePath = 'uploads/vinyls'
+const coverImageBasePath = 'uploads/albumCovers'
 
 const vinylSchema = new mongoose.Schema({
     title: {
